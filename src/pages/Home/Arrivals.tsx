9ef07211c82c25bdb4f7c4826479b7daef006388
minhthu0611 <21130555@st.hcmuaf.edu.vn>
2024-05-31 23:39:50 +0700

    add home page

--- src/pages/Home/Arrivals.tsx
+++ src/pages/Home/Arrivals.tsx
@@ -1,57 +1,25 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import listProduct from "../../api/products.json";
-import CardProduct from "../../components/CardProduct";
-import Title from "../../components/Title";
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-import { useEffect, useState } from 'react';
+import Products from '../../components/Products';
+import Title from '../../components/Title';
+import { Product } from '../../types/types'; // import Product from types.ts
 
-function Arrivals({}) {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
+interface ArrivalsProps {
+    data: Product[];
+}
 
-        window.addEventListener('resize', handleResize);
-
-        // Clean up event listener on component unmount
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-  return (
-    <div className="wrapper">
-      <div className="py-[64px]">
-        <Title className="text-center text-[32px] lg:text-[40px] mb-[64px]">NEW ARRIVALS</Title>
-        {/* <CardProduct data={data} /> */}
-        <Swiper
-                spaceBetween={20}
-                slidesPerView={windowWidth >= 1024 ? 4 : windowWidth >= 768 ? 3 : 2}
-                autoplay={{
-                    delay: 3000,
-                    disableOnInteraction: false,
-                }}
-                navigation={true}
-                modules={[Autoplay, Pagination, Navigation]}
-                className="!py-3 !px-20"
-            >
-                {listProduct?.map((product) => (
-                    <SwiperSlide key={product?.id}>
-                        <CardProduct data={product} />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        <div className="text-center mt-[36px] pb-[64px] border-b">
-          <button className="font-[Satoshi] px-[54px] py-4 border rounded-[62px] w-[200px] lg:w-auto">
-            View All
-          </button>
+const Arrivals: React.FC<ArrivalsProps> = ({ data }) => {
+    return (
+        <div className="wrapper">
+            <div className="py-[64px]">
+                <Title className="text-center text-[32px] lg:text-[40px] mb-[64px]">NEW ARRIVALS</Title>
+                <Products data={data} />
+                <div className="text-center mt-[36px] pb-[64px] border-b">
+                    <button className="font-[Satoshi] px-[54px] py-4 border rounded-[62px] w-full lg:w-auto">
+                        View All
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Arrivals;
---
+++ src/pages/Home/TopSelling.tsx
@@ -0,0 +1,24 @@
+import Products from '../../components/Products';
+import Title from '../../components/Title';
+import { Product } from '../../types/types'; // import Product from types.ts
+
+interface TopSellingProps {
+    data: Product[];
+}
+
+const TopSelling: React.FC<TopSellingProps> = ({ data }) => {
+    return (
+        <div className="wrapper">
+            <Title className="text-center text-[32px] lg:text-[40px] mb-[64px] uppercase">Top Selling</Title>
+
+            <Products data={data} />
+            <div className="text-center mt-[36px]">
+                <button className="font-[Satoshi] px-[54px] py-4 border rounded-[62px] w-full lg:w-auto  mb-[64px]">
+                    View All
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default TopSelling;
