9979cac28a8985961c29d17ca59890375843be4b
Vund <vund1@runsystem.net>
2024-06-02 22:05:22 +0700

    add image

--- src/components/Footer/FooterContent.tsx
+++ src/components/Footer/FooterContent.tsx
@@ -7,9 +7,9 @@ function FooterContent() {
     return (
         <div className="py-[50px] mt-0 border-b border-gray-300 flex lg:flex-row flex-col lg:gap-[110px] gap-6">
             <div className="w-[248px]">
-                <h2 className="font-[IntegralCf] text-[24px]">SEEDS</h2>
+                <h2 className="font-[IntegralCf] text-[24px]">SEEDLING</h2>
                 <p className="my-[30px]">
-                    Hạt giống là nơi bạn có thể tìm hạt giống cây trồng
+                    Là nơi bạn có thể tìm thấy những cây giống tốt nhất cho ngôi nhà của mình. Chúng tôi cung cấp những sản phẩm chất lượng nhất từ những nguồn cung cấp uy tín nhất.
                 </p>
                 <div className="flex gap-3">
                     <FaTwitter className="w-6 h-6" />
@@ -21,35 +21,34 @@ function FooterContent() {
 
             <div className="flex-1 grid lg:grid-cols-4 grid-cols-2 text-left gap-6">
                 <div className="space-y-4">
-                    <h2 className="tracking-widest uppercase font-[Satoshi]">Company</h2>
-                    <p>About</p>
+                    <h2 className="tracking-widest uppercase font-[Satoshi]">Công ty</h2>
+                    <p>Về chúng tôi</p>
                     <p>Features</p>
-                    <p>Works</p>
-                    <p>Career</p>
+                    <p>Tính năng</p>
+                    <p>Nghề nghiệp</p>
                 </div>
 
                 <div className="space-y-4">
-                    <h2 className="tracking-widest uppercase font-[Satoshi]">Help</h2>
-                    <p>Customer Support</p>
-                    <p>Delivery Details</p>
-                    <p>Terms & Conditions</p>
-                    <p>Privacy Policy</p>
+                    <h2 className="tracking-widest uppercase font-[Satoshi]">Hỗ trợ</h2>
+                    <p>Hỗ trợ khách hàng</p>
+                    <p>Chi tiết giao hàng</p>
+                    <p>Điều khoản sử dụng</p>
+                    <p>Chính sách</p>
                 </div>
 
                 <div className="space-y-4">
-                    <h2 className="tracking-widest uppercase font-[Satoshi]">FAQ</h2>
-                    <p>Account</p>
-                    <p>Manage Deliveries</p>
-                    <p>Orders</p>
-                    <p>Payments</p>
+                    <h2 className="tracking-widest uppercase font-[Satoshi]">Hỏi và trả lời</h2>
+                    <p>Tài khoản</p>
+                    <p>Quản lý việc giao hàng</p>
+                    <p>Đặt hàng</p>
+                    <p>Thanh toán</p>
                 </div>
 
                 <div className="space-y-4">
-                    <h2 className="tracking-widest uppercase font-[Satoshi]">Resources</h2>
-                    <p>Free eBooks</p>
-                    <p>Development Tutorial</p>
-                    <p>How to - Blog</p>
-                    <p>Youtube Playlist</p>
+                    <h2 className="tracking-widest uppercase font-[Satoshi]">Nhân lực</h2>
+                    <p>Hướng dẫn</p>
+                    <p>Blog</p>
+                    <p>Youtube</p>
                 </div>
             </div>
         </div>
--- src/components/Footer/FooterTop.tsx
+++ src/components/Footer/FooterTop.tsx
@@ -3,17 +3,17 @@ import { RiMailSendLine } from 'react-icons/ri';
 function FooterTop() {
     return (
         <div className="text-white flex lg:justify-between justify-center gap-[32px] text-center lg:text-left lg:flex-row flex-col py-[36px] px-[23px] lg:px-[64px] rounded-[20px] bg-black">
-            <p className="font-[IntegralCf] text-[35px]">STAY UPTO DATE ABOUT OUR LATEST OFFERS</p>
+            <p className="font-bold text-[35px]">CẬP NHẬT VỀ ƯU ĐÃI MỚI NHẤT CỦA CHÚNG TÔI</p>
             <div className="lg:min-w-[350px] relative">
                 <input
                     className="border outline-none w-full pl-[50px] pr-[16px] py-2 rounded-[20px] text-black"
-                    placeholder="Enter your email address"
+                    placeholder="Nhập địa chỉ email của bạn"
                 />
                 <span className="absolute top-0 left-[14px] py-2">
                     <RiMailSendLine className="w-6 h-7 text-gray-400" />
                 </span>
                 <button className="w-full bg-white text-black mt-3 rounded-[20px] py-3 font-[Satoshi]">
-                    Subscribe to Newsletter
+                    Đăng ký để nhận ưu đãi mới
                 </button>
             </div>
         </div>
