4ec1b2bed11eca1f1b73d293c86100e5a9fd559a
viet21092003 <21124548@st.hcmuaf.edu.vn>
2024-07-08 23:38:55 +0700

    config_token

--- fe/src/api/axios.ts
+++ fe/src/api/axios.ts
@@ -2,13 +2,13 @@ import axios from "axios";
 import { Login } from "../types/types";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:8081",
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080/api/v1/",
     headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
     },
+    // withCredentials: true
 })
-
 
 const navigate = (path: string) => {
     window.location.href = path;
